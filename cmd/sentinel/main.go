fd40dcaaec2f5bd7392387b17c89a7c45c267f90
Ayush Poudel <ayushpoudel@usf.edu>
2025-12-18 16:35:59 -0500

    Add http server struct and methods to start and shutdown with custom mux

---
+++ cmd/sentinel/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	srv := server.New(":8000")
+
+}
---
+++ internal/server/http.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+/*
+@ayuspoudel
+This will be our http utility function which allows us to initalize a http server
+It takes in port number and returns a Server struct with a custom handler initialized
+With 5s read, write and 30s idle timeout
+*/
+func New(addr string) *Server {
+	/*
+		@ayuspoudel
+		It is always safe to register a custom mux (http multiplexer) instead of using DefaultMux() or http.HandleFunc()
+		which internally uses a global mux. The problem with such mux is that when packages try to register endpoints in
+		your go program, it might be able to register an endpoint. Boom, you get an endpoint you never wanted. For such
+		reasons, it is safer to use a custom mux.
+	*/
+	mux := http.NewServeMux()
+
+	/*
+		@ayuspoudel
+		Registers a new /health endpoint which is very important for servers of this kind
+		It will indicate the current status of server by responing with "ok"
+		This can be useful for automation systems to parse and what not.
+	*/
+	mux.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+	return &Server{
+		httpServer: &http.Server{
+			Addr:         addr,
+			Handler:      mux,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 5 * time.Second,
+			IdleTimeout:  30 * time.Second,
+		},
+	}
+}
+
+/*
+	@ayuspoduel
+	Below are methods for struct Server. These will be used to start and shut down
+	the http servers
+*/
+
+func (s *Server) Start() error {
+	log.Printf("starting http server on %s\n", s.httpServer.Addr)
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	log.Println("shutting down server")
+	return s.httpServer.Shutdown(ctx)
+}
