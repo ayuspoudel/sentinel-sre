ca071fb989131ae9a1156bb3c4915563fb7e49b2
Ayush Poudel <ayushpoudel@usf.edu>
2025-12-22 03:21:37 -0500

    disable force http2 and increase maxConnections and maxIdleConnections to 1000 in http.transport

--- playground/load-generator/main.go
+++ playground/load-generator/main.go
@@ -15,19 +15,39 @@ var sentRequests uint64
 
 func main() {
 	targetURL := mustGetEnv("TARGET_URL")
-	if targetURL == "" {
-		log.Fatal("TARGET_URL environment variable is not set")
-	}
-
 	concurrency := getEnvInt("CONCURRENCY", 10)
 	sleepMs := getEnvInt("SLEEP_MS", 50)
 
-	transport := &http.Transport{
-		MaxIdleConns:        200,
-		MaxIdleConnsPerHost: 200,
-		IdleConnTimeout:     90 * time.Second,
+	/*
+		@ayuspoudel
+		http.Transport controls how aggresively conections are reused so load generation
+		stresses the service not TCP connection. Under the hood, it opens 200 open connections
+		and keep them idle. This has to be always greater than concurrency, if not connections will
+		be constantly opened and closed, making the client bottleneck instead of service.
+	*/
+	maxIdleConns := 1000
+	maxIdleConnsPerHost := 1000
+
+	if concurrency >= maxIdleConns {
+		maxIdleConns = concurrency + 50
+	}
+	if concurrency >= maxIdleConnsPerHost {
+		maxIdleConnsPerHost = concurrency + 50
 	}
 
+	transport := &http.Transport{
+		MaxIdleConns:        maxIdleConns,
+		MaxIdleConnsPerHost: maxIdleConnsPerHost,
+		IdleConnTimeout:     90 * time.Second,
+		DisableKeepAlives:   false,
+		ForceAttemptHTTP2:   false,
+	}
+	/*
+		@ayuspoudel
+		http client helps add policies and rules for any http request. We will be using
+		http.Get and using this client which has timeOut of 2s and a transport policy
+		we can establish http connections and do get requests on the target url.
+	*/
 	client := &http.Client{
 		Transport: transport,
 		Timeout:   2 * time.Second,
@@ -51,13 +71,14 @@ func main() {
 	wg.Wait()
 }
 
-func worker(
-	id int,
-	client *http.Client,
-	target string,
-	sleepMs int,
-	wg *sync.WaitGroup,
-) {
+/*
+@ayuspoudel
+Each worker thread will need to send one HTTP GET request. It needs to then
+wait for response or error. It needs to drain and close the response body,
+and also atomically increment the shared counter. It needs to sleep for a
+fixed amount of time and repeat forever.
+*/
+func worker(id int, client *http.Client, target string, sleepMs int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for {
@@ -72,11 +93,16 @@ func worker(
 		resp.Body.Close()
 
 		atomic.AddUint64(&sentRequests, 1)
-
+		/*
+			This ensures total [ load = concurrency * (1/(requestTime + sleep) ])
+		*/
 		time.Sleep(time.Duration(sleepMs) * time.Millisecond)
 	}
 }
 
+/*
+Ticks a timer of 5s, for everytime
+*/
 func logStats() {
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
