7805a506a30c7b4c3fb98bb23e753d0845124edd
Ayush Poudel <ayushpoudel@usf.edu>
2025-12-30 13:31:22 -0500

    Update engine to support repo polling and repo based manifest registry

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -3,21 +3,26 @@ package engine
 import (
 	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/ayuspoudel/sentinel-sre/internal/registry"
 )
 
 type Engine struct {
-	registry registry.Registry
-	interval time.Duration
-	guards   []registry.Guard
+	registry       registry.Registry
+	evalInterval   time.Duration
+	reloadInterval time.Duration
+
+	mu     sync.RWMutex
+	guards []registry.Guard
 }
 
-func New(reg registry.Registry, interval time.Duration) *Engine {
+func New(reg registry.Registry, evalInterval, reloadInterval time.Duration) *Engine {
 	return &Engine{
-		registry: reg,
-		interval: interval,
+		registry:       reg,
+		evalInterval:   evalInterval,
+		reloadInterval: reloadInterval,
 	}
 }
 
@@ -26,14 +31,19 @@ func (e *Engine) Start(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
 	e.guards = e.registry.Guards()
+	evalTicker := time.NewTicker(e.evalInterval)
+	reloadTicker := time.NewTicker(e.reloadInterval)
+	defer evalTicker.Stop()
+	defer reloadTicker.Stop()
 	log.Printf("engine started with %d guards", len(e.guards))
-	ticker := time.NewTicker(e.interval)
-	defer ticker.Stop()
 	for {
 		select {
-		case <-ticker.C:
-			e.runOnce(ctx)
+		case <-evalTicker.C:
+			e.evaluateOnce(ctx)
+		case <-reloadTicker.C:
+			e.tryReload(ctx)
 		case <-ctx.Done():
 			log.Println("engine stopped")
 			return nil
@@ -41,8 +51,33 @@ func (e *Engine) Start(ctx context.Context) error {
 	}
 }
 
-func (e *Engine) runOnce(ctx context.Context) {
+func (e *Engine) evaluateOnce(ctx context.Context) {
+	e.mu.RLock()
+	defer e.mu.RUnlock()
 	for _, g := range e.guards {
 		log.Printf("evaluating guard: %s", g.Name)
 	}
+}
+
+func (e *Engine) tryReload(ctx context.Context) {
+	log.Println("attempting registry reload")
+	err := e.reload(ctx)
+	if err != nil {
+		log.Printf("registry reload failed: %v (keeping previous state)", err)
+		return
+	}
+	log.Printf("registry reload succeeded, %d guards active", len(e.guards))
+
+}
+
+func (e *Engine) reload(ctx context.Context) error {
+	err := e.registry.Load(ctx)
+	if err != nil {
+		return err
+	}
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	e.guards = e.registry.Guards()
+	return nil
+
 }
