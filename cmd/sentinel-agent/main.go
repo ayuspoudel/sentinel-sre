2341fe39306709594c57e6bf312e8d5a44806898
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-05 10:48:14 -0500

    Add sentinel agent code

---
+++ cmd/sentinel-agent/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ayuspoudel/sentinel-sre/agent/admission"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	// Admission server
+	srv := admission.NewServer(":8443")
+
+	go func() {
+		log.Println("sentinel-agent admission server starting on :8443")
+		if err := srv.Start(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("agent server error: %v", err)
+		}
+	}()
+
+	<-stop
+	log.Println("sentinel-agent shutting down")
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("shutdown error: %v", err)
+	}
+}
