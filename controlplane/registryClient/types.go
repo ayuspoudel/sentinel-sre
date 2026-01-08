8b63419b43517caf0085715f58ecbfddc09523d7
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-08 17:27:37 -0500

    Add registry client to provide interface to talk to registry

--- controlplane/registryClient/types.go
+++ controlplane/registryClient/types.go
@@ -4,7 +4,7 @@ package registryClient
 This is not a DB model, but it is what registry will expose over the wire
 */
 type Cluster struct {
-	Name       string
-	Credential string
-	Labels     map[string]string
+	Name          string            `json:"name"`
+	CredentialRef string            `json:"credential_ref"`
+	Labels        map[string]string `json:"labels"`
 }
