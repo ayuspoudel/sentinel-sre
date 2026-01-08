334f7d7da55c9b6a56e88f8bc4904247fd6e1165
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-08 15:12:02 -0500

    Add main go file with mux and concurrent workers to serve cluster registration

--- cmd/sentinel-cluster-registry/main.go
+++ cmd/sentinel-cluster-registry/main.go
@@ -1,1 +1,35 @@
-package sentinelclusterregistry
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/ayuspoudel/sentinel-sre/controlplane/cluster"
+)
+
+func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL is required")
+	}
+
+	ctx := context.Background()
+
+	db, err := cluster.NewDB(ctx, dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = cluster.RunMigrations(ctx, db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store := cluster.NewPgxStore(db)
+	handler := cluster.NewHandler(store)
+	server := cluster.NewServer(":8080", handler)
+
+	log.Fatal(server.Run())
+}
