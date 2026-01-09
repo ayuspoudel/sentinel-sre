bf9d306ad28c6dd2ba0c11c2d9cf05da9a7b06aa
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-09 10:55:20 -0500

    Add logic to write kubernetes kubeconfig as a secret in cluster

---
+++ controlplane/cluster/secrets.go
@@ -0,0 +1,37 @@
+package cluster
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func EnsureKubeconfigSecret(ctx context.Context, client *kubernetes.Clientset, namespace, name string, kubeconfig []byte) error {
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			"kubeconfig": kubeconfig,
+		},
+	}
+	_, err := client.CoreV1().Secrets(namespace).Create(ctx, secret, metav1.CreateOptions{})
+	if err != nil {
+		if apierrors.IsAlreadyExists(err) {
+			_, updateErr := client.CoreV1().Secrets(namespace).Update(ctx, secret, metav1.UpdateOptions{})
+			if updateErr != nil {
+				return fmt.Errorf("failed to update existing kubeconfig: %w", updateErr)
+			}
+		} else {
+			return fmt.Errorf("failed to ensure kubeconfig secret: %w", err)
+		}
+	}
+	return nil
+
+}
