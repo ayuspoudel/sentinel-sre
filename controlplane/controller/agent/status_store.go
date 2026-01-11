e70fa048da7154c8ba5f34f267aefa76e43a72e7
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-11 13:56:32 -0500

    remove legacy status store interface after refactor

--- controlplane/controller/agent/status_store.go
+++
@@ -1,8 +0,0 @@
-package agent
-
-import "context"
-
-type Store interface {
-	Upsert(ctx context.Context, st *ClusterStatus) error
-	Get(ctx context.Context, clusterName string) (*ClusterStatus, error)
-}
