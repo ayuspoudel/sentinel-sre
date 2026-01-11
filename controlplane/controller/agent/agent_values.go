48e8a0a9d5a2cc7db2a85a54cda9c38e3ed1ac83
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-11 12:02:33 -0500

    add AgentValues, SentinelConfig, and ClusterConfig types for agent configuration

---
+++ controlplane/controller/agent/agent_values.go
@@ -0,0 +1,14 @@
+package agent
+
+type AgentValues struct {
+	SentinelSRE SentinelConfig `json:"sentinelSRE"`
+	Cluster     ClusterConfig  `json:"cluster"`
+}
+
+type SentinelConfig struct {
+	URL string `json:"url"`
+}
+
+type ClusterConfig struct {
+	Name string `json:"name"`
+}
