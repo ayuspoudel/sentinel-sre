4a0adfb29ec34a5896da38324f017868c915bf4c
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-11 12:02:29 -0500

    implement BuildAgentValues function for agent configuration

---
+++ controlplane/controller/agent/agent_values_builder.go
@@ -0,0 +1,12 @@
+package agent
+
+func BuildAgentValues(controlPlaneURL, clusterName string) map[string]interface{} {
+	return map[string]interface{}{
+		"sentinelSRE": map[string]interface{}{
+			"url": controlPlaneURL,
+		},
+		"cluster": map[string]interface{}{
+			"name": clusterName,
+		},
+	}
+}
