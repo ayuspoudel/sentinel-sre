e6948a104575e5fe872e70d4bccaddbd5330b363
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-12 12:47:11 -0500

    add documentation comments for ClusterRegistryAdapter

--- controlplane/policy/adapters/cluster_registry.go
+++ controlplane/policy/adapters/cluster_registry.go
@@ -5,6 +5,14 @@ import (
 	"database/sql"
 )
 
+/*
+	Author: @ayuspoudel
+	This is a cluster registry adaptor. It assumes that cluster registry and policy registry will share the same DB
+	and cluster registry's table is named clusers.
+	We want to get information from cluster registry about reachability of cluster, health of agent and so on.
+
+*/
+
 type ClusterRegistryAdapter struct {
 	db *sql.DB
 }
