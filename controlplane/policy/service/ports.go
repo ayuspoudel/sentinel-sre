95d2b0e1a428067ce19bf902eab2e4045b08c0bf
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-12 12:47:22 -0500

    add documentation comments for service ports interface

--- controlplane/policy/service/ports.go
+++ controlplane/policy/service/ports.go
@@ -2,6 +2,24 @@ package service
 
 import "context"
 
+/*
+	Author: @ayuspoudel
+	These are ports. These are interface definintions which we allow such that we
+	can support this `implicity interface satisfaction`. What we do is this is defined
+	in the service layer, and implemented in the adapter layer. At runtime we create
+	the AdapterLayer interfaces by providing the inputs they need and finally pass it onto
+	the service layer.
+	Ex:
+		in main.go
+		pgDB := setupDB()
+		clusterRegistryAdapter := adapters.NewClusterRegistryAdapter(pgDB)
+		controllerReaderAdapter := adapters.NewControllerReaderAdapter(pgDB)
+		promClientAdapter := adapters.NewPrometheusClientAdapter(promConfig)
+			|
+			|
+		registryService := service.NewRegistryService(store, clusterRegistryAdapter, controllerReaderAdapter, promClientAdapter)
+*/
+
 type ClusterRegistry interface {
 	ClusterExists(ctx context.Context, name string) (bool, error)
 }
