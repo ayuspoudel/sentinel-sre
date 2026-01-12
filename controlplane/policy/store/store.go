b5266d48d713ca4988de98931360d01e54500967
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-12 01:01:24 -0500

    add PolicyStore interface for policy management operations

---
+++ controlplane/policy/store/store.go
@@ -0,0 +1,18 @@
+package store
+
+import (
+	"context"
+
+	"github.com/ayuspoudel/sentinel-sre/controlplane/policy/spec"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/policy/status"
+)
+
+type PolicyStore interface {
+	UpsertPolicy(ctx context.Context, policy *spec.PolicySpec) error
+	GetPolicy(ctx context.Context, name string) (*spec.PolicySpec, error)
+	ListPolicies(ctx context.Context) ([]*spec.PolicySpec, error)
+	DeletePolicy(ctx context.Context, name string) error
+
+	GetStatus(ctx context.Context, policyName string) (*status.PolicyStatus, error)
+	UpdateStatus(ctx context.Context, st *status.PolicyStatus) error
+}
