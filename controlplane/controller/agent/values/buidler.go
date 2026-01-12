613ffc3988b5101debfbad3077ff823c0b695e3f
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-11 19:12:14 -0500

    Override image name and tag in values file

--- controlplane/controller/agent/values/buidler.go
+++ controlplane/controller/agent/values/buidler.go
@@ -1,12 +1,16 @@
 package values
 
-func BuildAgentValues(controlPlaneURL, clusterName string) map[string]interface{} {
+func BuildAgentValues(controlPlaneURL, clusterName, imageRepo, imageTag string) map[string]interface{} {
 	return map[string]interface{}{
 		"sentinelSRE": map[string]interface{}{
 			"url": controlPlaneURL,
 		},
 		"cluster": map[string]interface{}{
 			"name": clusterName,
+		},
+		"image": map[string]interface{}{
+			"repository": imageRepo,
+			"tag":        imageTag,
 		},
 	}
 }
