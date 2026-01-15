0efb870c8ed981add1bd074f2905e45e23f73fe6
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-14 21:14:45 -0500

    Add Cluster struct to define cluster properties and metadata

---
+++ .archive/cluster.archive/model.go
@@ -0,0 +1,10 @@
+package cluster
+
+import "time"
+
+type Cluster struct {
+	Name          string            `json:"name" db:"name"`
+	CredentialRef string            `json:"credential_ref" db:"credential_ref"`
+	Labels        map[string]string `json:"labels" db:"labels"`
+	CreatedAt     time.Time         `json:"created_at" db:"created_at"`
+}
