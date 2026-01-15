b9dbf645fb5ebecd2d0f8391115c7bcb0a4b7cff
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-14 19:24:51 -0500

    Arhive

--- .archive/cluster/api.go
+++
@@ -1,7 +0,0 @@
-package cluster
-
-type RegisterRequest struct {
-	Name          string            `json:"name"`
-	CredentialRef string            `json:"credential_ref"`
-	Labels        map[string]string `json:"labels"`
-}
