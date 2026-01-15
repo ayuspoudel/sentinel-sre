47f5a464cb2caa6a48ad1b62b4c5d886b71d9a80
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-14 21:15:42 -0500

    Refactor Controller to remove registryClient dependency and improve cluster management

--- controlplane/agent-controller/controller/controller.go
+++ controlplane/agent-controller/controller/controller.go
@@ -4,27 +4,29 @@ import (
 	"context"
 	"time"
 
-	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/events"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/events/clusterStatusEvent"
 	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/install"
 	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/logging"
-	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/status"
-	"github.com/ayuspoudel/sentinel-sre/controlplane/registryClient"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/models/clusterStatusModel"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/store/clusterRegistry"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/agent-controller/store/clusterStatus"
 	"k8s.io/client-go/kubernetes"
 )
 
 type Controller struct {
-	registry        *registryClient.Client
 	kubeClient      *kubernetes.Clientset
-	store           status.Store
+	store           clusterStatus.Store
+	clusterStatus   clusterStatusModel.ClusterStatus
+	clusterRegistry clusterRegistry.Store
 	interval        time.Duration
 	controlPlaneUrl string
 	installer       install.Installer
-	publisher       events.ClusterStatusPublisher
+	publisher       clusterStatusEvent.ClusterStatusPublisher
 }
 
-func NewController(registry *registryClient.Client, kubeClient *kubernetes.Clientset, store status.Store, interval time.Duration, controlPlaneUrl string, installer install.Installer,
-	publisher events.ClusterStatusPublisher) *Controller {
-	return &Controller{registry: registry, kubeClient: kubeClient, store: store, interval: interval, controlPlaneUrl: controlPlaneUrl, installer: installer,
+func NewController(kubeClient *kubernetes.Clientset, store clusterStatus.Store, clusterRegistry clusterRegistry.Store, interval time.Duration, controlPlaneUrl string, installer install.Installer,
+	publisher clusterStatusEvent.ClusterStatusPublisher) *Controller {
+	return &Controller{kubeClient: kubeClient, store: store, clusterRegistry: clusterRegistry, interval: interval, controlPlaneUrl: controlPlaneUrl, installer: installer,
 		publisher: publisher}
 }
 
@@ -47,14 +49,14 @@ func (c *Controller) Run(ctx context.Context) {
 
 func (c *Controller) reconcileOnce(ctx context.Context) {
 	log := logging.From(ctx)
-	clusters, err := c.registry.ListClusters(ctx)
+	clusters, err := c.clusterRegistry.List(ctx)
 	if err != nil {
 		log.Error("error listing clusters", "error", err.Error())
 
 		return
 	}
 	for _, cluster := range clusters {
-		clusterCtx := logging.With(ctx, "cluster", cluster.Name, "credential_ref", cluster.CredentialRef)
+		clusterCtx := logging.With(ctx, "cluster", cluster.ClusterName, "credential_ref", cluster.CredentialRef)
 		c.reconcileCluster(clusterCtx, cluster)
 	}
 }
