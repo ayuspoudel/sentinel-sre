02fd46bfd37ff53b4ccc4832eebef0812221038f
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-23 10:49:30 -0500

    Add RedisPublisher for publishing cluster intent events

---
+++ controlplane/cluster/events/redis/publisher.go
@@ -0,0 +1,85 @@
+package redisPublisher
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/ayuspoudel/sentinel-sre/controlplane/cluster/events"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/cluster/events/clusterDeleted"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/cluster/events/clusterRegistered"
+	"github.com/ayuspoudel/sentinel-sre/controlplane/cluster/model"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisPublisher struct {
+	client     *redis.Client
+	streamName string
+}
+
+var _ events.ClusterIntentPublisher = (*RedisPublisher)(nil)
+
+/*
+	Author: @ayuspoudel
+	This publishes cluster intent events emitted by cluster registry.
+	It is best-effort and non-blocking.
+*/
+
+func NewRedisPublisher(client *redis.Client, streamName string) *RedisPublisher {
+	return &RedisPublisher{client: client, streamName: streamName}
+}
+
+func (p *RedisPublisher) PublishClusterRegistered(ctx context.Context, c *model.Cluster) error {
+	if c == nil {
+		return fmt.Errorf("nil cluster status")
+	}
+	clusterData := clusterRegistered.FromCluster(c)
+	event := clusterRegistered.NewClusterDataEvent(clusterData)
+	payload, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("failed to marshal cluster registered event: %w", err)
+	}
+	args := &redis.XAddArgs{Stream: p.streamName, ID: "*", Values: map[string]interface{}{
+		"type":      event.Type,
+		"source":    event.Source,
+		"payload":   string(payload),
+		"schema":    event.SchemaVersion,
+		"timestamp": event.Timestamp.Format(time.RFC3339),
+	},
+	}
+	err = p.client.XAdd(ctx, args).Err()
+	if err != nil {
+		return fmt.Errorf("failed to publish cluster registered event to redis: %w", err)
+	}
+	return nil
+
+}
+
+func (p *RedisPublisher) PublishClusterDeleted(ctx context.Context, clusterName string) error {
+	if clusterName == "" {
+		return fmt.Errorf("empty cluster name")
+	}
+	event := clusterDeleted.NewClusterDeletedEvent(clusterName)
+	payload, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("failed to marshal cluster deleted event: %w", err)
+	}
+	args := &redis.XAddArgs{
+		Stream: p.streamName,
+		ID:     "*",
+		Values: map[string]interface{}{
+			"type":      event.Type,
+			"source":    event.Source,
+			"payload":   string(payload),
+			"schema":    event.SchemaVersion,
+			"timestamp": event.TimeStamp.Format(time.RFC3339),
+		},
+	}
+	err = p.client.XAdd(ctx, args).Err()
+	if err != nil {
+		return fmt.Errorf("failed to publish cluster deleted event to redis: %w", err)
+	}
+	return nil
+
+}
