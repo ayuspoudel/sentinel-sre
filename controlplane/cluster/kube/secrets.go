57e3d5c39224370c9029374e2e69a32425fe173e
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-23 10:49:43 -0500

    Refactor EnsureKubeconfigSecret to use KubeClient receiver and update DeleteKubeconfigSecret implementation

--- controlplane/cluster/kube/secrets.go
+++ controlplane/cluster/kube/secrets.go
@@ -7,10 +7,9 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 )
 
-func EnsureKubeconfigSecret(ctx context.Context, client *kubernetes.Clientset, namespace, name string, kubeconfig []byte) error {
+func (k *KubeClient) EnsureKubeconfigSecret(ctx context.Context, namespace, name string, kubeconfig []byte) error {
 	secret := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
@@ -21,10 +20,10 @@ func EnsureKubeconfigSecret(ctx context.Context, client *kubernetes.Clientset, n
 			"kubeconfig": kubeconfig,
 		},
 	}
-	_, err := client.CoreV1().Namespaces().Get(ctx, namespace, metav1.GetOptions{})
+	_, err := k.client.CoreV1().Namespaces().Get(ctx, namespace, metav1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
-			_, err = client.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{
+			_, err = k.client.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: namespace,
 				},
@@ -34,10 +33,10 @@ func EnsureKubeconfigSecret(ctx context.Context, client *kubernetes.Clientset, n
 			return fmt.Errorf("failed to ensure namespace for kubeconfig secret: %w", err)
 		}
 	}
-	_, err = client.CoreV1().Secrets(namespace).Create(ctx, secret, metav1.CreateOptions{})
+	_, err = k.client.CoreV1().Secrets(namespace).Create(ctx, secret, metav1.CreateOptions{})
 	if err != nil {
 		if apierrors.IsAlreadyExists(err) {
-			_, updateErr := client.CoreV1().Secrets(namespace).Update(ctx, secret, metav1.UpdateOptions{})
+			_, updateErr := k.client.CoreV1().Secrets(namespace).Update(ctx, secret, metav1.UpdateOptions{})
 			if updateErr != nil {
 				return fmt.Errorf("failed to update existing kubeconfig: %w", updateErr)
 			}
@@ -47,4 +46,20 @@ func EnsureKubeconfigSecret(ctx context.Context, client *kubernetes.Clientset, n
 	}
 	return nil
 
+}
+
+/*
+Author: @ayuspoudel
+This function is needed when user intends to remove a cluster from sentinel
+Sentinel must ensure that the kubeconfigsecret is removed
+*/
+func (k *KubeClient) DeleteKubeconfigSecret(ctx context.Context, namespace, name string) error {
+	err := k.client.CoreV1().Secrets(namespace).Delete(ctx, name, metav1.DeleteOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return fmt.Errorf("failed to delete kubeconfig secret %s: %w", name, err)
+	}
+	return nil
 }
